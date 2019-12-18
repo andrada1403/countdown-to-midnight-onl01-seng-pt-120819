81e66d79d95bccdc8ca1e58d5c471a31ef72a7e8
Andrada Lorena Popa <andrada.lorena.popa@gmail.com>
2019-12-18 19:46:21 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,14 @@
 #write your code here
+def countdown(counter)
+  while counter>=1 || counter==10
+    puts "#{counter} SECOND(S)!"
+    counter-=1
+  end
+"HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep(x)
+  until x==5
+    true
+  end
 end
